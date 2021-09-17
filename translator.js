4d7f4b6e3a9666e1cd44e4600030c1f5e80883c0
aliabdellatif0 <ali.abdlaitf@gmail.com>
2021-09-17 12:14:32 +0100

    TDD approach taken first

--- translator.js
+++ translator.js
@@ -1,3 +1,7 @@
-export const translator = letters => {
-    
+import { alphabet } from "./data"
+import { capAlphabet } from "./data"
+import { morseCode } from "./data"
+
+export const translator = (letters) => {
+
 }
--- translator.test.js
+++ translator.test.js
@@ -1,7 +1,87 @@
 //it should 
-//take the letter a and out put the morse code which is .-
+//take the letter a and output the morse code which is .-
+
+import { translator } from "./translator";
+
+//it should
+//give an error when
 
 //it should
 
-//it should
+describe("testing transformInputToArray()", ()=> {
+    //valid test case
+    it("Should take an input of 'abcd' and give ['a', 'b', 'c', 'd']", () => {
+        //arrange
+        let result;
+        //act
+        result = transformInputToArray("abcd");
+        //assert -> test will pass or fail
+        expect(result).toBe(['a', 'b', 'c', 'd']);
+    })
+}
+
+
+describe("testing translator()", ()=> {
+    //valid test case
+    it("Should take an input of a or A and return .-", () => {
+        //arrange
+        let result;
+        //act
+        result = translator("a");
+        //assert -> test will pass or fail
+        expect(result).toBe(".-");
+    })
+
+    it("Should take an input of a or A and return .-", () => {
+        //arrange
+        let result;
+        //act
+        result = translator("A");
+        //assert -> test will pass or fail
+        expect(result).toBe(".-");
+    })
+    
+    it("Should take an input of 1 and return .----", () => {
+            //arrange
+    let result;
+            //act
+    result = translator("1");
+            //assert -> test will pass or fail
+    expect(result).toBe(".----");
+    })
+
+    it("Should output an alert message when inputting '@' ", () => {
+        //arrange
+    let result;
+        //act
+    result = translator("@");
+        //assert -> test will pass or fail
+    expect(result).toBe("Invalid, please try again");
+    })
+}
+
+
+describe("testing morseOutput()", ()=> {
+    //valid test case
+    it(" Should join together an array of morse coded inputs", () => {
+        //arrange
+        let result = morseOutput([".-", ".--.", "..."])
+        //act
+        
+        //assert -> test will pass or fail
+        expect(result).toBe("aps");
+    })
+
+    it("Should use a slash as MORSE code for spaces inputted", () => {
+        //arrange
+        let result = morseOutput([".-", ".--.", " ", ".---", ".-."])
+        //act
+        
+        //assert -> test will pass or fail
+        expect(result).toBe("ap/jr");
+    })
+
+
+      
+
 
